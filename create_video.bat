71199aa0c69cc1f1ae44202a4fb836a6d8949ceb
Speedy_Lex <alex.ciocildau@gmail.com>
2024-06-27 10:40:07 +0200

    materials + cam settings (WIP)

--- create_video.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-ffmpeg -framerate 30 -i frames\img%%d.png -c:v libx264 -pix_fmt yuv420p video.mp4
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{mem::size_of, ptr::slice_from_raw_parts, sync, thread};
+use std::{fs::File, io::Write, ptr::slice_from_raw_parts};
 
 use ash::vk::{BufferCopy, CommandBufferBeginInfo, CommandBufferResetFlags, FenceCreateInfo, PipelineBindPoint, StructureType, SubmitInfo};
 use image::Rgba;
+use rand::RngCore;
 use vulkan::device::buffer::{StagedSSBO, StagedUBO};
 
 pub mod vulkan;
@@ -11,17 +12,18 @@ const HEIGHT: usize = 1200;
 
 #[repr(C)]
 struct UBOData {
-    size: [u32; 8],
-    pos: [f64; 2],
-    zoom: f64,
-    gamma: f64
+    size: [u32; 4],
+    cam: [f32; 8],
+    spheres: Vec<f32>,
 }
 impl<'a> UBOData {
-    fn new(size: [usize; 2], pos: [f64; 2], zoom: f64, gamma: f64) -> Self {
-        Self { size: [size[0] as u32, size[1] as u32, 0, 0, 0, 0, 0, 0], pos: [pos[0], pos[1]], zoom, gamma }
+    fn new(size: [usize; 2], spheres: Vec<f32>, count: usize, seed: u32, vfov: f32, lookfrom: [f32; 3], lookat: [f32; 3]) -> Self {
+        Self { size: [size[0] as u32, size[1] as u32, count as u32, seed], spheres, cam: [lookfrom[0], lookfrom[1], lookfrom[2], vfov.to_radians(), lookat[0], lookat[1], lookat[2], 0.0]}
     }
-    fn bytes(&'a self) -> &'a [u8] {
-        unsafe { &*slice_from_raw_parts(self as *const Self as *const u8, size_of::<Self>()) }
+    fn vec(&self) -> Vec<u8> {
+        let mut vec = unsafe { &*slice_from_raw_parts(self as *const Self as *const u8, 16) }.to_vec();
+        vec.extend_from_slice(unsafe { &*slice_from_raw_parts(self.spheres.as_ptr() as *const u8, 4*self.spheres.len()) });
+        vec
     }
 }
 
@@ -30,12 +32,14 @@ fn main() {
     let physical = vulkan::device::PhysicalDevice::find_device(&vk).unwrap().unwrap();
     let logical = vulkan::device::LogicalDevice::create_logical_device(physical);
     let queue = logical.create_queue();
-    //-1.768800555, -0.001768198
-    //-0.7454607800914831, 0.09635969753181201
-    //0.1360592161152, 0.6257839882281
-    let mut ubodata = UBOData::new([WIDTH, HEIGHT], [-0.63981440099510262, -0.40987916004273033], 0.0000001, 2.0);
-    let mut ubo = StagedUBO::new(&logical, ubodata.bytes().to_vec());
-    let mut ssbo = StagedSSBO::<u8>::new(&logical, WIDTH * HEIGHT * 4);
+
+    let mut dat = vec![0.0, 0.0, -1.0, 0.5, 0.0, -100.5, -1.0, 100.0, -1.0, 0.0, -1.0, 0.5, 1.0, 0.0, -1.0, 0.5];
+    dat.resize(64*4, 0.0);
+    dat.extend_from_slice(&[0.1, 0.2, 0.5, f32::INFINITY, 0.8, 0.8, 0.0, f32::INFINITY, 1.5, f32::INFINITY, f32::INFINITY, f32::INFINITY, 0.8, 0.6, 0.2, 1.0]);
+    let mut ubodata = UBOData::new([WIDTH, HEIGHT], dat, 4, rand::thread_rng().next_u32(), 30.0, [0.0, 0.0, 0.0], [0.0, 0.0, -1.0]);
+
+    let mut ubo = StagedUBO::new(&logical, ubodata.vec());
+    let mut ssbo = StagedSSBO::<f32>::new(&logical, WIDTH * HEIGHT * 4);
     let pipe = vulkan::device::shaders::Pipeline::new("shader.spv", &ubo, &ssbo, &logical);
     let cmd = logical.create_command_buffer();
     let info = CommandBufferBeginInfo {
@@ -48,19 +52,11 @@ fn main() {
     };
     let fence = unsafe { logical.device.create_fence(&fence, None) }.unwrap();
     
-    let (send, recv) = sync::mpsc::sync_channel(5);
-    let recv = recv;
-    let thread = thread::spawn(move || {
-        for (x, image) in recv {
-            image::ImageBuffer::<Rgba<u8>, _>::from_vec(WIDTH as u32, HEIGHT as u32, image).unwrap().save(format!("frames\\img{x}.png")).map_err(|x| x.to_string()).unwrap();
-        }
-    });
+    let samples = 50;
+    for x in 0usize..samples {
+        ubodata.size[3] = rand::thread_rng().next_u32();
+        ubo.get_slice().copy_from_slice(&ubodata.vec());
 
-    let frames = 500;
-    for x in 0usize..frames {
-        ubodata.gamma = lerp(0.5, 2.0, x as f64/(frames-1) as f64);
-        ubodata.zoom = lerp(3.0f64.ln(), 0.00000000001f64.ln(), x as f64/(frames-1) as f64).exp();
-        ubo.get_slice().copy_from_slice(&ubodata.bytes());
         unsafe { logical.device.begin_command_buffer(cmd, &info) }.unwrap();
         unsafe { logical.device.cmd_copy_buffer(cmd, ubo.get_stage(), ubo.get_ubo(), &[BufferCopy { size: ubo.get_size() as u64, ..Default::default() }]) };
         unsafe { logical.device.end_command_buffer(cmd) }.unwrap();
@@ -128,17 +124,13 @@ fn main() {
         unsafe { logical.device.wait_for_fences(&[fence], true, u64::MAX) }.unwrap();
         unsafe { logical.device.reset_fences(&[fence]) }.unwrap();
         unsafe { logical.device.queue_wait_idle(queue.queue) }.unwrap();
-        let mut image = Vec::with_capacity(WIDTH*HEIGHT*4);
-        unsafe { image.set_len(ssbo.get_slice().len()) };
-        image.copy_from_slice(ssbo.get_slice());
-        send.send((x, image)).unwrap();
+        if x%10 == 0 {
+            println!("{x}");
+        }
     }
+    println!("{:?}", &ssbo.get_slice()[0..20]);
+    let image = Vec::from_iter(ssbo.get_slice().iter().map(|x| (*x*255.0/samples as f32) as u8));
+    image::ImageBuffer::<Rgba<u8>, _>::from_vec(WIDTH as u32, HEIGHT as u32, image).unwrap().save(format!("img.png")).map_err(|x| x.to_string()).unwrap();
 
     unsafe { logical.device.destroy_fence(fence, None) };
-    drop(send);
-    thread.join().unwrap();
-}
-
-fn lerp(start: f64, end: f64, t: f64) -> f64 {
-    (end-start)*t + start
 }
